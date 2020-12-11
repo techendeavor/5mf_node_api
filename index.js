d3cd231865e4d0b36ac6bbc1b2f99eb61e0cee19
Clif Hodges <chodges@Clifs-MBP.austin.rr.com>
2020-12-10 22:35:47 -0600

    initialize

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const data = require('./dummy')
+
+const api = express()
+
+const HOST = 'localhost'
+const PORT = 8888
+
+api.get('/', (req,res) => {
+    res.send('Welcome to this awesome API!')
+})
+
+api.get('/people', (req,res) => {
+    res.status(200).json(data)
+})
+
+
+api.listen(PORT, () => console.log(`API running at ${HOST}:${PORT}!`))
